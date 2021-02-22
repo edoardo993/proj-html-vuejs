a2bed64654c111a70d66cf5ac4d5c12105ea7a4a
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-02-22 08:50:04 +0100

    js started

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 new Vue({
-    el: '#app'
-})
+    el: '#app',
+    data: {
+        navbarListItems: [
+
+            {name: 'Home'},
+            {name: 'Recipes'},
+            {name: 'Places'},
+            {name: 'Blog'},
+            {name: 'About'},
+            {name: 'Contact'},
+            {name: '<i class="fas fa-search"></i>'}
+      
+        ]
+    },
+    methods: {
+        
+    }
+});
+Vue.config.devtools=true;
