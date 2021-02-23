3c212de74af5b960dfbb50b89df589bc751900dd
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-02-23 15:45:25 +0100

    js comments

--- js/script.js
+++ js/script.js
@@ -2,8 +2,13 @@ new Vue({
     el: '#app',
     data: {
         
+        // input utente per l'iscrizione al sito nel footer
         userEmailInput: '',
 
+        // lista link footer
+        footerNavList: [],
+
+        // array oggetti navbar
         navbarListItems: [
 
             {name: 'Home', url:'#'},
@@ -14,6 +19,8 @@ new Vue({
             {name: 'Contact', url: '#'}
       
         ],
+
+        // array oggetti url-foto/titolo sezione foodie journal
         foodieJournalPhotoContainer: [
             {
                 url: 'img/single-post-img3.jpg',
@@ -28,6 +35,8 @@ new Vue({
                 title: 'Why These Toasts with Tea are My New Favorite'
             }
         ],
+
+        // array oggetti url-foto/descrizione sezione popular recipes
         popularRecipesImgsRight: [
             {
                 url: 'img/Mixed-fruits-460x295.jpg',
@@ -62,77 +71,8 @@ new Vue({
                 description: 'Strawberry Fruit Pies Served With Tea'
             }
         ],
-        miniaturesContainer: [
-            {
-                url: 'img/fi-roundup-400x263.jpg',
-                description: 'Roundup: My New Favourite Recipes For Healthy Living'
-            },
-            {
-                url: 'img/fi-korean-food-400x263.jpg',
-                description: 'Meal Prep: Korean Bibimbap with Kimchi'
-            },
-            {
-                url: 'img/fi-toasts-400x263.jpg',
-                description: 'Why These Toasts with Tea are My New Favorite'
-            },
-            {
-                url: 'img/fi-street-food-400x263.jpg',
-                description: 'Exploring Street Food in Bangkok'
-            },
-            {
-                url: 'img/fi-organic-breakfast-400x263.jpg',
-                description: 'Organic Choices For Healthier Living'
-            },
-            {
-                url: 'img/fi-water-side-rest-400x263.jpg',
-                description: '5 Waterside Restaurants in Istanbul for Special Events'
-            },
-        ],
-        footerImgs: [
-            {
-                url: 'img/Yogurt-Nan-400x263.jpg',
-                description: 'Lunch Favourite with Salad, Naan And Beans'
-            },
-            {
-                url: 'img/fi-korean-food-400x263.jpg',
-                description: 'Fruit Platter with Banana, Mango, Berries and Orange'
-            },
-            {
-                url: 'img/fi-toasts-400x263.jpg',
-                description: 'Breakfast Delight With Strawberry, Egg and Fruit'
-            },
-            {
-                url: 'img/fi-street-food-400x263.jpg',
-                description: 'Ice Cream Heaven With Vanilla, Chocolate And Pistachio'
-            }
-        ],
-        socialIcons: [
-            {
-                name: 'facebook',
-                link: 'https://www.facebook.com',
-                class: 'fab fa-facebook-f'
-            },
-            {
-                name: 'instagram',
-                link: 'https://www.instagram.com',
-                class: 'fab fa-instagram'
-            },
-            {
-                name: 'twitter',
-                link: 'https://www.twitter.com',
-                class: 'fab fa-twitter'
-            },
-            {
-                name: 'youtube',
-                link: 'https://www.youtube.com',
-                class: 'fab fa-youtube'
-            },
-            {
-                name: 'pinterest',
-                link: 'https://www.pinterest.com',
-                class: 'fab fa-pinterest'
-            }
-        ],
+
+        // array oggetti url-foto/descrizione sezione culinary collection
         culinarySectionImgs: [
             {
                 url: 'img/drinks-recipes.png',
@@ -167,8 +107,87 @@ new Vue({
                 description: 'quick & easy'
             }
         ],
-        footerNavList: []
+
+        // array oggetti url-foto/descrizione sezione miniatures container
+        miniaturesContainer: [
+            {
+                url: 'img/fi-roundup-400x263.jpg',
+                description: 'Roundup: My New Favourite Recipes For Healthy Living'
+            },
+            {
+                url: 'img/fi-korean-food-400x263.jpg',
+                description: 'Meal Prep: Korean Bibimbap with Kimchi'
+            },
+            {
+                url: 'img/fi-toasts-400x263.jpg',
+                description: 'Why These Toasts with Tea are My New Favorite'
+            },
+            {
+                url: 'img/fi-street-food-400x263.jpg',
+                description: 'Exploring Street Food in Bangkok'
+            },
+            {
+                url: 'img/fi-organic-breakfast-400x263.jpg',
+                description: 'Organic Choices For Healthier Living'
+            },
+            {
+                url: 'img/fi-water-side-rest-400x263.jpg',
+                description: '5 Waterside Restaurants in Istanbul for Special Events'
+            },
+        ],
+
+        // array oggetti url-foto/descrizione sezione immagini footer
+        footerImgs: [
+            {
+                url: 'img/Yogurt-Nan-400x263.jpg',
+                description: 'Lunch Favourite with Salad, Naan And Beans'
+            },
+            {
+                url: 'img/fi-korean-food-400x263.jpg',
+                description: 'Fruit Platter with Banana, Mango, Berries and Orange'
+            },
+            {
+                url: 'img/fi-toasts-400x263.jpg',
+                description: 'Breakfast Delight With Strawberry, Egg and Fruit'
+            },
+            {
+                url: 'img/fi-street-food-400x263.jpg',
+                description: 'Ice Cream Heaven With Vanilla, Chocolate And Pistachio'
+            }
+        ],
+
+        // array oggetti nome/link/classe icone social
+        socialIcons: [
+            {
+                name: 'facebook',
+                link: 'https://www.facebook.com',
+                class: 'fab fa-facebook-f'
+            },
+            {
+                name: 'instagram',
+                link: 'https://www.instagram.com',
+                class: 'fab fa-instagram'
+            },
+            {
+                name: 'twitter',
+                link: 'https://www.twitter.com',
+                class: 'fab fa-twitter'
+            },
+            {
+                name: 'youtube',
+                link: 'https://www.youtube.com',
+                class: 'fab fa-youtube'
+            },
+            {
+                name: 'pinterest',
+                link: 'https://www.pinterest.com',
+                class: 'fab fa-pinterest'
+            }
+        ]
     },
+
+    // mounted per avere la lista dei link del footer, eliminando la
+    // voce Home dall'elenco
     mounted(){
         let navList=[];
         this.navbarListItems.forEach((el)=>{
@@ -179,18 +198,30 @@ new Vue({
         return this.footerNavList=navList
     },
     methods: {
+
+        // funzione per mostrare div con display:none quando si va in hover
+        // su un'immagine (con classe comune, per div con più immagini)
         viewHoverCommonClass(commonClass, index){
             let hoverDiv=document.getElementsByClassName(commonClass)[index];
             hoverDiv.classList.remove('hid')
         },
+
+        // funzione per nascondere div con display:block quando lasciamo l'hover
+        // da un'immagine (con classe comune, per div con più immagini)
         hideHoverCommonClass(commonClass, index){
             let hoverDiv=document.getElementsByClassName(commonClass)[index];
             hoverDiv.classList.add('hid')
         },
+
+        // funzione per mostrare div con display:none quando si va in hover
+        // su un'immagine (con ID, per div con una sola immagine)
         viewHoverId(idName){
             let hoverDiv=document.getElementById(idName);
             hoverDiv.classList.remove('hid')
         },
+
+        // funzione per nascondere div con display:block quando lasciamo l'hover
+        // da un'immagine (con ID, per div con una sola immagine)
         hideHoverId(idName){
             let hoverDiv=document.getElementById(idName);
             hoverDiv.classList.add('hid')
